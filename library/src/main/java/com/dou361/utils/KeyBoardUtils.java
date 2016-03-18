551467798c45f25f124678f2d502fc21e257bb54
“chenguanming” <chen_guanming@eluxue.net>
2016-03-18 18:17:30 +0800

    添加软键盘管理

--- library/src/main/java/com/dou361/utils/KeyBoardUtils.java
+++ library/src/main/java/com/dou361/utils/KeyBoardUtils.java
@@ -1,6 +1,8 @@
 package com.dou361.utils;
 
+import android.app.Activity;
 import android.content.Context;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
@@ -55,5 +57,42 @@ public class KeyBoardUtils {
                 .getSystemService(Context.INPUT_METHOD_SERVICE);
 
         imm.hideSoftInputFromWindow(mEditText.getWindowToken(), 0);
+    }
+
+    /**
+     * 设置输入法,如果当前页面输入法打开则关闭
+     * @param activity
+     */
+    public static void hideInputMethod(Activity activity){
+        View a = activity.getCurrentFocus();
+        if(a != null){
+            InputMethodManager imm = (InputMethodManager) UIUtils.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            try {
+                imm.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 强制显示输入法
+     * @param view
+     */
+    public static void showInputMethod(View view){
+        try {
+            InputMethodManager imm = (InputMethodManager) UIUtils.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(view,InputMethodManager.SHOW_FORCED);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /** 手动打开或关闭软键盘，如果打开就关闭，如果关闭就打开 */
+    private static void toggleSoftInput() {
+        // 隐藏输入法
+        InputMethodManager imm = (InputMethodManager) UIUtils.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        // 显示或者隐藏输入法
+        imm.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
     }
 }
